e86fd87f8de69a0d80986d9f13f7cb3b5cc39018
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-20 09:02:05 -0800

    update "Program.cs"

--- src/Andtech.Todo.Console/Program.cs
+++ src/Andtech.Todo.Console/Program.cs
@@ -4,101 +4,160 @@ using Spectre.Console;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 public class Program
 {
 
-    static Table table;
-
-    public static async Task Main(string[] args)
-    {
-        Session.Instance = new Session()
-        {
-            ProjectDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "todo"),
-        };
-        var session = Session.Instance;
-        session.Lists.Add(TodoList.Read(session.ProjectDir + "/todo.md"));
-
-        var thread = new Thread(InputLoop);
-        thread.Start();
-
-        await DoGuiAsync();
-    }
-
-    static void InputLoop()
+	public class TaskRenderer
 	{
-        while (true)
-        {
-            var keyInfo = AnsiConsole.Console.Input.ReadKey(true);
-            if (keyInfo.HasValue)
-            {
-                //AnsiConsole.MarkupLine($"{keyInfo.Value.Modifiers} + {keyInfo.Value.Key}");
+		private readonly TodoTask task;
 
-                if (keyInfo.Value.Modifiers == 0)
+		public TaskRenderer(TodoTask task)
+		{
+			this.task = task;
+		}
+
+		public void Submit()
+		{
+			task.Complete = !task.Complete;
+		}
+
+		public string Render()
+		{
+			var symbol = task.Complete ? "☒" : "☐";
+			var text = string.Join(" ",
+				symbol,
+				task.Title,
+				task.Description
+				);
+			var markup = $"{text}".EscapeMarkup();
+
+			return markup;
+		}
+	}
+
+	static Table table;
+
+	private static List<TaskRenderer> elements;
+	private static Task mainTask;
+
+	public static async Task Main(string[] args)
+	{
+		Session.Instance = new Session()
+		{
+			ProjectDir = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "todo"),
+		};
+		var session = Session.Instance;
+		session.Lists.Add(TodoList.Read(session.ProjectDir + "/todo.md"));
+
+		var thread = new Thread(InputLoop);
+		thread.Start();
+
+		await StartGuiLoop();
+	}
+
+	static void InputLoop()
+	{
+		while (true)
+		{
+			var keyInfo = AnsiConsole.Console.Input.ReadKey(true);
+			if (keyInfo.HasValue)
+			{
+				//AnsiConsole.MarkupLine($"{keyInfo.Value.Modifiers} + {keyInfo.Value.Key}");
+
+				if (keyInfo.Value.Modifiers == 0)
 				{
 					switch (keyInfo.Value.Key)
 					{
-                        case ConsoleKey.DownArrow:
-                            lineNumber++;
-                            table.Rows.Update(lineNumber, 0, new Text("DOWN\n  description"));
-                            break;
-                        case ConsoleKey.UpArrow:
-                            lineNumber--;
-                            table.Rows.Update(lineNumber, 0, new Text("UP"));
-                            break;
-                    }
+						case ConsoleKey.DownArrow:
+							lineNumber++;
+							break;
+						case ConsoleKey.UpArrow:
+							lineNumber--;
+							break;
+						case ConsoleKey.Spacebar:
+							elements[lineNumber].Submit();
+							break;
+						case ConsoleKey.Enter:
+							break;
+					}
 				}
 				else
 				{
-
+					switch (keyInfo.Value.Modifiers)
+					{
+						case ConsoleModifiers.Control:
+							if (keyInfo.Value.Key == ConsoleKey.Q)
+							{
+								Console.Clear();
+								Environment.Exit(0);
+							}
+							break;
+					}
 				}
-            }
-        }
+			}
+		}
 	}
 
-    static int lineNumber = 0;
+	static int lineNumber = 0;
 
-    static async Task DoGuiAsync()
-    {
-        AnsiConsole.Clear();
-        AnsiConsole.Write(new Rule("[white]General[/]"));
+	static Task StartGuiLoop()
+	{
+		AnsiConsole.Clear();
+		AnsiConsole.Write(new Rule("[white]General[/]"));
 
-        table = new Table().Expand().BorderColor(Color.Grey).NoBorder().HideHeaders();
-            table.AddColumn("Content");
+		var taskList = Session.Instance.Lists[0];
+		elements = new List<TaskRenderer>();
+		foreach (var item in taskList.Tasks)
+		{
+			var element = new TaskRenderer(item);
+			elements.Add(element);
+		}
 
-        int n = 8;
+		int n = elements.Count;
+		table = new Table()
+			.Expand()
+			.NoBorder()
+			.HideHeaders();
+		table.AddColumn("Content");
+		foreach (var renderer in elements)
+		{
+			var text = renderer.Render();
 
-        await AnsiConsole.Live(table)
-            .AutoClear(true)
-            .Overflow(VerticalOverflow.Ellipsis)
-            .Cropping(VerticalOverflowCropping.Bottom)
-            .StartAsync(async ctx =>
-            {
-                var taskList = Session.Instance.Lists[0];
-                foreach (var item in taskList.Tasks)
-                {
-                    var symbol = item.Complete ? "✓" : " ";
-                    var text = string.Join(" ",
-                        $"[{symbol}]",
-                        item.Title,
-                        item.Description
-                    );
-                    var markup = $"• {text}".EscapeMarkup();
+			table.AddRow(text);
+		}
 
-                    table.AddRow(markup);
-                }
+		return AnsiConsole.Live(table)
+			.AutoClear(true)
+			.Overflow(VerticalOverflow.Ellipsis)
+			.Cropping(VerticalOverflowCropping.Bottom)
+			.StartAsync(RefreshAsync);
+	}
 
-                // Continously update the table
-                while (true)
-                {
-                    // Refresh and wait for a while
-                    ctx.Refresh();
-                    await Task.Delay(16);
-                }
-            });
-    }
+	static async Task RefreshAsync(LiveDisplayContext context)
+	{
+		// Continously update the table
+		while (true)
+		{
+			// Refresh and wait for a while
+			context.Refresh();
+			await Task.Delay(16);
+
+			for (int i = 0; i < table.Rows.Count; i++)
+			{
+				var row = i + 0;
+
+				var text = elements[row].Render();
+				if (row == lineNumber)
+				{
+					text = $"[blue]{text}[/]";
+				}
+
+				table.Rows.Update(row, 0, new Markup(text));
+			}
+		}
+	}
 }
 
