cffa2809d1075e63828b76658137ce91b8460d7d
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-20 09:34:29 -0800

    update "SpectreExtensions.cs", "Andtech.Todo.Console.csproj", "Andtech.Todo.c...

---
+++ src/Andtech.Todo.Console/SpectreExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Spectre.Console.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andtech.Todo
+{
+
+	internal sealed class ControlCode : Renderable
+	{
+		private readonly Segment _segment;
+
+		public ControlCode(string control)
+		{
+			_segment = Segment.Control(control);
+		}
+
+		protected override Measurement Measure(Spectre.Console.Rendering.RenderOptions options, int maxWidth)
+		{
+			return new Measurement(0, 0);
+		}
+
+		protected override IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
+		{
+			if (options.Ansi)
+			{
+				yield return _segment;
+			}
+		}
+	}
+
+	internal class SpectreExtensions
+	{
+	}
+}
