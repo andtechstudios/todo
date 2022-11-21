71df214e53eed03b9e038faa080a8a92a2345a6b
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-20 18:10:06 -0800

    lazy
    Very hard api

--- src/Andtech.Todo.Console/Program.cs
+++ src/Andtech.Todo.Console/Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Terminal.Gui;
 
 public partial class Program
 {
@@ -22,9 +23,10 @@ public partial class Program
 
 		var token = cts.Token;
 
-		await SpectreExtensions.AlternateScreenAsync(AnsiConsole.Console, () => RunAsync(cancellationToken: token));
-
+		//await SpectreExtensions.AlternateScreenAsync(AnsiConsole.Console, () => RunAsync(cancellationToken: token));
 		//await RunAsync(cancellationToken: token);
+
+		Tui();
 
 		cts.Cancel();
 		cts.Dispose();
@@ -39,6 +41,54 @@ public partial class Program
 		var session = Session.Instance;
 		session.Lists.Add(TodoList.Read(session.ProjectDir + "/todo.md"));
 	}
+
+	static void Tui()
+	{
+		Application.Init();
+		Edit("README.md");
+
+		static void Edit(string filename)
+		{
+			var top = new Toplevel()
+			{
+				X = 0,
+				Y = 0,
+				Width = Dim.Fill(),
+				Height = Dim.Fill()
+			};
+			var menu = new MenuBar(new MenuBarItem[] {
+			new MenuBarItem ("_File", new MenuItem [] {
+				new MenuItem ("_Close", "", () => {
+					Application.RequestStop ();
+				})
+			}),
+		});
+
+			// nest a window for the editor
+			var win = new Window(filename)
+			{
+				X = 0,
+				Y = 1,
+				Width = Dim.Fill(),
+				Height = Dim.Fill() - 1
+			};
+
+			var editor = new TextView()
+			{
+				X = 0,
+				Y = 0,
+				Width = Dim.Fill(),
+				Height = Dim.Fill()
+			};
+			editor.Text = System.IO.File.ReadAllText(filename);
+			win.Add(editor);
+
+			// Add both menu and win in a single call
+			top.Add(win, menu);
+			Application.Run(top);
+			Application.Shutdown();
+		}
+	}	
 
 	static async Task RunAsync(CancellationToken cancellationToken)
 	{
