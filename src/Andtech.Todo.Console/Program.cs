f4e593432d1d889b511525483fbcda91ced407ff
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-21 22:20:28 -0800

    rearrange

--- src/Andtech.Todo.Console/Program.cs
+++ src/Andtech.Todo.Console/Program.cs
@@ -7,35 +7,6 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using static Crayon.Output;
-
-public class TaskPrinter : Printer
-{
-	public override string Text => text;
-
-	private readonly TodoTask task;
-	private string text;
-
-	public TaskPrinter(TodoTask task)
-	{
-		this.task = task;
-	}
-
-	public override void Rebuild(int width)
-	{
-		var symbol = task.IsCompleted ? "☒" : "☐";
-		var content = task.Title;
-		if (task.IsCompleted)
-		{
-			content = Dim(content);
-		}
-		text = string.Join(" ",
-			symbol,
-			content,
-			task.Description
-			);
-	}
-}
 
 public partial class Program
 {
---
+++ src/Andtech.Todo.Console/TaskPrinter.cs
@@ -0,0 +1,31 @@
+﻿using Andtech.Todo;
+using static Crayon.Output;
+
+public class TaskPrinter : Printer
+{
+	public override string Text => text;
+
+	private readonly TodoTask task;
+	private string text;
+
+	public TaskPrinter(TodoTask task)
+	{
+		this.task = task;
+	}
+
+	public override void Rebuild(int width)
+	{
+		var symbol = task.IsCompleted ? "☒" : "☐";
+		var content = task.Title;
+		if (task.IsCompleted)
+		{
+			content = Dim(content);
+		}
+		text = string.Join(" ",
+			symbol,
+			content,
+			task.Description
+			);
+	}
+}
+
