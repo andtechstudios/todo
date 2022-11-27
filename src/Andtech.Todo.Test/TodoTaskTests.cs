0497e8e4f178521794a685ee235e3b2fca273df1
ZenSharp <48300131+zensharp@users.noreply.github.com>
2022-11-26 19:09:58 -0800

    add tests

--- src/Andtech.Todo.Test/TodoTaskTests.cs
+++ src/Andtech.Todo.Test/TodoTaskTests.cs
@@ -47,5 +47,55 @@ namespace Andtech.Todo.Test
 			Assert.That(task.Title, Is.EqualTo("Call Saul"));
 			Assert.That(task.Description, Is.EqualTo("visit [Saul's Website](http://bettercallsaul.amc.com/) for details."));
 		}
+
+		[Test]
+		public void ParseWithMarkdown()
+		{
+			var task = TodoTask.Parse("* Call Saul: it *is* **important** that `we` speak to him urgently");
+			Assert.That(task.Title, Is.EqualTo("Call Saul"));
+			Assert.That(task.Description, Is.EqualTo("it *is* **important** that `we` speak to him urgently"));
+		}
+
+		[Test]
+		public void ParseTags()
+		{
+			var task = TodoTask.Parse("* Call Saul #work #legal");
+			CollectionAssert.AreEqual(new string[] { "work", "legal" }, task.Tags);
+		}
+
+		[Test]
+		public void ParseDate()
+		{
+			var task = TodoTask.Parse("* Call Saul due:2009-04-26");
+			Assert.That(DateOnly.FromDateTime(task.DueDate), Is.EqualTo(DateOnly.Parse("2009-04-26")));
+		}
+
+		[Test]
+		public void ParseAssignees()
+		{
+			var task = TodoTask.Parse("* Call Saul @me @jpinkman @swhite");
+			CollectionAssert.AreEqual(new string[] { "me", "jpinkman", "swhite" }, task.Assignees);
+		}
+
+		[Test]
+		public void ParseMultiple()
+		{
+			var task = TodoTask.Parse("* Call Saul #work #legal due:2009-04-26 @me @jpinkman @swhite");
+			CollectionAssert.AreEqual(new string[] { "work", "legal" }, task.Tags);
+			Assert.That(DateOnly.FromDateTime(task.DueDate), Is.EqualTo(DateOnly.Parse("2009-04-26")));
+			CollectionAssert.AreEqual(new string[] { "me", "jpinkman", "swhite" }, task.Assignees);
+		}
+
+		[Test]
+		public void ParseComplete()
+		{
+			var task = TodoTask.Parse("* [x] Call Saul: better call saul #work due:2009-04-26 @jpinkman");
+			Assert.That(task.IsCompleted, Is.EqualTo(true));
+			Assert.That(task.Title, Is.EqualTo("Call Saul"));
+			Assert.That(task.Description, Is.EqualTo("better call saul"));
+			CollectionAssert.AreEqual(new string[] { "work" }, task.Tags);
+			Assert.That(DateOnly.FromDateTime(task.DueDate), Is.EqualTo(DateOnly.Parse("2009-04-26")));
+			CollectionAssert.AreEqual(new string[] { "jpinkman" }, task.Assignees);
+		}
 	}
 }
